2786e09669b7f9995bb14f692e7f6678da0051e1
tomiris.rakhymzhan <tomiris.rakhymzhan@gmail.com>
2022-04-20 06:11:24 +0100

    ShoppingCart for Customer Area

--- BulkyBookWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkyBookWeb/Areas/Customer/Controllers/HomeController.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using BulkyBook.Models;
+using BulkyBook.DataAccess.Repository.IRepository;
+//using BulkyBook.Models;
 
 namespace BulkyBookWeb.Controllers
 {
@@ -14,14 +16,30 @@ namespace BulkyBookWeb.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IUnitOfWork _unitOfWork;
+
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category,CoverType");
+
+            return View(productList);
+        }
+
+        public IActionResult Details(int id)
+        {
+            ShoppingCart cartObj = new()
+            {
+                Count = 1,
+                Product = _unitOfWork.Product.GetFirstOrDefault(u => u.Id == id, includeProperties: "Category,CoverType"),
+            };
+
+            return View(cartObj);
         }
 
         public IActionResult Privacy()
