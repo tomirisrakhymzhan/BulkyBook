a1d0a7ecb24755542013b6d4d0648d8b59989834
tomiris.rakhymzhan <tomiris.rakhymzhan@gmail.com>
2022-04-21 07:55:42 +0100

    Re-pushing ignored Identity razor pages and BulkyBook.Utility project

---
+++ BulkyBook.Utility/SD.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BulkyBook.Utility
+{
+    public static class SD
+    {
+        public const string Role_User_Indi = "Individual";
+        public const string Role_User_Comp = "Company";
+        public const string Role_Admin = "Admin";
+        public const string Role_Employee = "Employee";
+    }
+}
